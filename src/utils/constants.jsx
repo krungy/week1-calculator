5bd82e840c9c09a029a9897d9be0577eeef6d0dd
고동현 <dhjk35@naver.com>
2022-01-25 11:53:24 +0900

    ✨ feat: 계산기에 따른 API 분리

--- src/utils/constants.jsx
+++ src/utils/constants.jsx
@@ -1,5 +1,10 @@
 const { REACT_APP_API_KEY } = process.env;
+const calcOneCountries = ["KRW", "JPY", "PHP"];
+const calcTwoCountries = ["USD", "CAD", "KRW", "HKD", "JPY", "CNY"];
 
 export const API_POINT = 'http://www.apilayer.net/api/live';
 
 export const DEFAULT_URL = API_POINT + '?access_key=' + REACT_APP_API_KEY;
+
+export const CALC_ONE_COUNTRIES_URL = API_POINT + '?access_key=' + REACT_APP_API_KEY + '&currencies=' + calcOneCountries.join(",");
+export const CALC_TWO_COUNTRIES_URL = API_POINT + '?access_key=' + REACT_APP_API_KEY + '&currencies=' + calcTwoCountries.join(",");
