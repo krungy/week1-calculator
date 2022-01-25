d3e3a482a011c6181967df544de1d5ae76b683f1
krungy <furler711@gmail.com>
2022-01-25 23:44:32 +0900

    🚑 hotfix: api endpoint 수정

--- src/utils/constants.jsx
+++ src/utils/constants.jsx
@@ -1,8 +1,8 @@
 const { REACT_APP_API_KEY } = process.env;
-const calcOneCountries = ["KRW", "JPY", "PHP"];
+const calcOneCountries = ['KRW', 'JPY', 'PHP'];
 export const SECOND_COUNTRY_LIST = ['USD', 'CAD', 'KRW', 'HKD', 'JPY', 'CNY'];
 
-export const API_POINT = 'http://www.apilayer.net/api/live';
+export const API_POINT = 'http://api.currencylayer.com/live';
 
 export const MONTHS = [
   'Jan',
@@ -19,5 +19,15 @@ export const MONTHS = [
   'Dec',
 ];
 
-export const CALC_ONE_COUNTRIES_URL = API_POINT + '?access_key=' + REACT_APP_API_KEY + '&currencies=' + calcOneCountries.join(",");
-export const CALC_TWO_COUNTRIES_URL = API_POINT + '?access_key=' + REACT_APP_API_KEY + '&currencies=' + SECOND_COUNTRY_LIST.join(",");
+export const CALC_ONE_COUNTRIES_URL =
+  API_POINT +
+  '?access_key=' +
+  REACT_APP_API_KEY +
+  '&currencies=' +
+  calcOneCountries.join(',');
+export const CALC_TWO_COUNTRIES_URL =
+  API_POINT +
+  '?access_key=' +
+  REACT_APP_API_KEY +
+  '&currencies=' +
+  SECOND_COUNTRY_LIST.join(',');
