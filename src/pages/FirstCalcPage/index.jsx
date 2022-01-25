2ffd5a76b275fd148c7c635e6005bfea3a092529
krungy <furler711@gmail.com>
2022-01-25 20:12:35 +0900

    ✨ feat: FirstCalcPage 기능 구현 완성

--- src/pages/FirstCalcPage/index.jsx
+++ src/pages/FirstCalcPage/index.jsx
@@ -1,93 +1,175 @@
-import React from 'react';
+import React, { useCallback, useEffect, useState } from 'react';
 import styled from 'styled-components';
+import { requestCalcOne } from '@api/api';
+import { calcOneCountryList } from '@utils/calcOneCountry';
 
-const dummyData = [
-  {
-    title: "송금국가",
-    value: "미국(USD)",
-    status: "fixed"
-  },
-  {
-    title: "수취국가",
-    value: "",
-    status: "select"
-  },
-  {
-    title: "환율",
-    value: "1193.93 KRW/USD",
-    status: "fixed"
-  },
-  {
-    title: "송금액",
-    value: "USD",
-    status: "input"
-  }
-];
-
-const dummySelect = [
-  {
-    value: "1",
-    country: "한국(KRW)"
-  },
-  {
-    value: "2",
-    country: "일본(JPY)"
-  },
-  { 
-    value: "3",
-    country: "필리핀(PHP)"
-  }
-]
-
-const HeaderContainer = styled.h1`
-  font-size: 24px;
-  margin-bottom: 14px;
+const FirstCalcPageContainer = styled.div`
+  position: fixed;
+  top: 50%;
+  left: 50%;
+  width: 400px;
+  height: 520px;
+  transform: translate(-50%, -50%);
+  background-color: white;
+  box-shadow: rgb(0 0 0 / 30%) 0px 12px 40px -12px;
+  border-radius: 12px;
+  box-sizing: border-box;
+  padding: 38px 48px;
+  text-align: center;
+  word-break: keep-all;
 `;
 
-const ExchangeList = styled.div`
+const HeaderContainer = styled.h1`
+  text-align: center;
+  font-size: 24px;
+  margin-bottom: 36px;
+`;
+
+const ContentContainer = styled.div`
   display: flex;
+  flex-direction: column;
+  gap: 14px;
+`;
+
+const ItemContainer = styled.div`
+  position: relative;
+  display: flex;
+  gap: 4px;
+  align-items: center;
+  width: 100%;
+`;
+
+const SelectContainer = styled.select`
+  display: inline-flex;
   flex-direction: column;
 `;
 
-const ListContent = styled.div`
-  line-height: 1.4;
-`;
+const InputContainer = styled.input``;
 
-const SubmitBtn = styled.button`
+const ButtonContainer = styled.button`
   display: block;
-  width: 100px;
+  width: 68px;
   height: 24px;
-  margin-top: 8px;
+  margin: 0 auto;
+  margin-top: 36px;
+  background-color: #24a19c;
+  color: #fff;
+  border: none;
+  border-radius: 4px;
+  cursor: pointer;
+  &:active {
+    opacity: 0.6;
+  }
 `;
 
 const ResultContainer = styled.div`
-  margin-top: 32px;
-`
+  margin-top: 82px;
+`;
 
 const FirstCalcPage = () => {
-  const getOption = () => dummySelect.map((item, idx) => {
-    return (
-      <option key={idx} value={item.value}>{item.country}</option>
-    )
-  });
+  const [initialData, setInitialData] = useState('');
+  const [isLoading, setIsLoading] = useState(false);
+  const [currentCountry, setCurrentCountry] = useState('USDKRW');
+  const [inputValue, setInputValue] = useState('');
+  const [isSubmit, setIsSubmit] = useState(false);
 
-  const getList = () => dummyData.map((item, idx) => {
-    return (
-      <ListContent>
-        <span>{item.title}: </span>
-        <span>{item.value}</span>
-        { item.status === "select" ? <select value={"1"}>{getOption()}</select> : item.status === "input" ? <input /> : null }
-      </ListContent>
-    )
-  })
+  useEffect(() => {
+    const getApi = async () => {
+      try {
+        setIsLoading(true);
+        const data = await requestCalcOne();
+        setInitialData(data.quotes);
+        setIsLoading(false);
+      } catch (e) {
+        console.log(e);
+      }
+    };
+    getApi();
+  }, []);
 
+  const handleDropdownOnChange = useCallback((e) => {
+    setCurrentCountry(e.target.value);
+    setIsSubmit(false);
+  }, []);
+
+  const handleDropdown = (list) => {
+    const entries = Object.entries(list);
+    const menuList = entries.map(([key, _], index) => (
+      <option value={key} key={index}>
+        {calcOneCountryList[key]}
+      </option>
+    ));
+    return (
+      <SelectContainer defaultValue="USDKOR" onChange={handleDropdownOnChange}>
+        {menuList}
+      </SelectContainer>
+    );
+  };
+
+  const handleCurrentExchange = useCallback(() => {
+    return (
+      <span>
+        {Number(initialData[currentCountry]).toFixed(2)}{' '}
+        {currentCountry.slice(3, 6)}/{currentCountry.slice(0, 3)}
+      </span>
+    );
+  }, [initialData, currentCountry]);
+
+  const handleSubmit = useCallback(() => {
+    setIsSubmit(true);
+  }, []);
+
+  const handleOnChange = useCallback((e) => {
+    setIsSubmit(false);
+    setInputValue(e.target.value);
+  }, []);
+
+  const handleInputContainer = useCallback(() => {
+    return (
+      <InputContainer
+        type="number"
+        value={inputValue}
+        onChange={(e) => handleOnChange(e)}
+      />
+    );
+  }, [handleOnChange, inputValue]);
+
+  const handleResultContainer = useCallback(() => {
+    const calculatedValue = (
+      Number(initialData[currentCountry]) * Number(inputValue)
+    ).toFixed(2);
+    const resultPrice = calculatedValue
+      .toString()
+      .replace(/\B(?<!\.\d*)(?=(\d{3})+(?!\d))/g, ',');
+
+    return (
+      <ResultContainer>
+        {inputValue === ''
+          ? '올바른 송금액을 입력해주세요.'
+          : `수취금액은 ${resultPrice} ${currentCountry.slice(3, 6)} 입니다.`}
+      </ResultContainer>
+    );
+  }, [initialData, currentCountry, inputValue]);
+
+  console.log(initialData);
   return (
-    <>
+    <FirstCalcPageContainer>
       <HeaderContainer>환율 계산</HeaderContainer>
-      <ExchangeList>{getList()}</ExchangeList>
-      <SubmitBtn>Submit</SubmitBtn>
-      <ResultContainer>수취금액은 111993.00 KRW입니다.</ResultContainer>
-    </>
+      <ContentContainer>
+        <ItemContainer>송금국가: 미국(USD)</ItemContainer>
+        <ItemContainer>
+          수취국가: {!isLoading && handleDropdown(initialData)}
+        </ItemContainer>
+        <ItemContainer>
+          환율: {!isLoading && handleCurrentExchange()}
+        </ItemContainer>
+        <ItemContainer>
+          송금액: {!isLoading && handleInputContainer()} USD
+        </ItemContainer>
+        <ButtonContainer onClick={handleSubmit}>Submit</ButtonContainer>
+        {isSubmit && handleResultContainer()}
+      </ContentContainer>
+    </FirstCalcPageContainer>
   );
 };
 
